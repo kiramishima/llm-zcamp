0065491ce48c3fcfe00e689e81af9ca16450781d
Kira Mishima <kiramishima@outlook.com>
2024-07-22 23:15:40 -0600

    update repo

--- Workshops/rest_api_pipeline.py
+++ Workshops/rest_api_pipeline.py
@@ -82,6 +82,8 @@ def rest_api_notion_resource():
 
     yield from rest_api_source(notion_config,name="employee_handbook")
 
+
+
 def extract_page_content(response):
     block_id = response["id"]
     last_edited_time = response["last_edited_time"]
@@ -110,10 +112,76 @@ def extract_page_content(response):
 def rest_api_notion_incremental(
     last_edited_time = dlt.sources.incremental("last_edited_time", initial_value="2024-06-26T08:16:00.000Z",primary_key=("block_id"))
 ):
-    # last_value = last_edited_time.last_value
-    # print(last_value)
+    last_value = last_edited_time.last_value
+    print(last_value)
 
     for block in rest_api_notion_resource.add_map(extract_page_content):
+        if not(len(block["content"])):
+            continue
+        yield block
+
+@dlt.resource(name="homework")
+def rest_api_notion_homework_resource():
+    notion_config: RESTAPIConfig = {
+        "client": {
+            "base_url": "https://api.notion.com/v1/",
+            "auth": {
+                "token": dlt.secrets["sources.rest_api.notion.api_key"]
+            },
+            "headers":{
+            "Content-Type": "application/json",
+            "Notion-Version": "2022-06-28"
+            }
+        },
+        "resources": [
+            {
+                "name": "search",
+                "endpoint": {
+                    "path": "search",
+                    "method": "POST",
+                    "paginator": PostBodyPaginator(),
+                    "json": {
+                        "query": "homework",
+                        "sort": {
+                            "direction": "ascending",
+                            "timestamp": "last_edited_time"
+                        }
+                    },
+                    "data_selector": "results"
+                }
+            },
+            {
+                "name": "page_content",
+                "endpoint": {
+                    "path": "blocks/{page_id}/children",
+                    "paginator": JSONResponsePaginator(),
+                    "params": {
+                        "page_id": {
+                            "type": "resolve",
+                            "resource": "search",
+                            "field": "id"
+                        }
+                    },
+                }
+            }
+        ]
+    }
+
+    yield from rest_api_source(notion_config, name="homework")
+
+@dlt.resource(
+    name="homework",
+    write_disposition="merge",
+    primary_key="block_id",
+    columns={"last_edited_time":{"dedup_sort":"desc"}}
+    )
+def rest_api_notion_homework_incremental(
+    last_edited_time = dlt.sources.incremental("last_edited_time", initial_value="2024-06-26T08:16:00.000Z", primary_key=("block_id"))
+):
+    last_value = last_edited_time.last_value
+    print(last_value)
+
+    for block in rest_api_notion_homework_resource.add_map(extract_page_content):
         if not(len(block["content"])):
             continue
         yield block
@@ -136,6 +204,24 @@ def load_notion() -> None:
     )
     print(load_info)
 
+def load_homework() -> None:
+    pipeline = dlt.pipeline(
+        pipeline_name="company_homework",
+        destination="lancedb",
+        dataset_name="notion_homework",
+        # full_refresh=True
+    )
+
+    load_info = pipeline.run(
+        lancedb_adapter(
+            rest_api_notion_homework_incremental,
+            embed="content"
+        ),
+        table_name="homework",
+        write_disposition="merge"
+    )
+    print(load_info)
 
 if __name__ == "__main__":
-    load_notion()
+    # load_notion()
+    load_homework()
